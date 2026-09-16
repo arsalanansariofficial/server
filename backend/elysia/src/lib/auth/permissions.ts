a54cc308f85e3d3dc221bf677b19fe935df7093c
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-09-16 07:25:11 +0530

    Add organization roles and permissions to access controller

--- backend/elysia/src/lib/auth/permissions.ts
+++ backend/elysia/src/lib/auth/permissions.ts
@@ -1,15 +1,34 @@
 import {
-  defaultStatements,
-  defaultRoles
+  defaultStatements as defaultOrganizationStatements,
+  defaultRoles as defaultOrganizationRoles
+} from 'better-auth/plugins/organization/access';
+import {
+  defaultStatements as defaultAdminStatements,
+  defaultRoles as defaultAdminRoles
 } from 'better-auth/plugins/admin/access';
 import { createAccessControl } from 'better-auth/plugins/access';
 
+export type Permissions = Partial<{
+  [K in keyof typeof ac.statements]: (typeof ac.statements)[K][number][];
+}>;
+export type Roles = keyof typeof permissions.roles;
+
 const ac = createAccessControl({
-  ...defaultStatements,
   task: ['create', 'read', 'update', 'delete'],
+  ...defaultOrganizationStatements,
+  ...defaultAdminStatements
 });
 
 const user = ac.newRole({ task: ['create', 'read', 'update', 'delete'] });
 const admin = ac.newRole(ac.statements);
 
-export const permissions = { roles: { ...defaultRoles, admin, user }, ac };
+export const permissions = {
+  roles: {
+    ...defaultOrganizationRoles,
+    ...defaultAdminRoles,
+    owner: admin,
+    admin,
+    user
+  },
+  ac
+};
