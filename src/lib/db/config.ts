28200e712867d9b24957bc5055d0b92f4548d42f
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-09-24 19:21:37 +0530

    Change default name for drizzle migration table to '__migrations__'

--- src/lib/db/config.ts
+++ src/lib/db/config.ts
@@ -4,6 +4,7 @@ import { env } from '@/lib/config';
 
 export default defineConfig({
   dbCredentials: { url: env.DATABASE_URL },
+  migrations: { table: '__migrations__' },
   schema: 'src/lib/db/schema.ts',
   out: 'src/lib/db/migrations',
   dialect: 'sqlite'
